e6b8a9c0021869968956f08ed11ff85ad5820043
Sun <klsforever@gmail.com>
2016-09-13 11:51:30 +0800

    增加返回值类型的测试用例

--- test/spec/config-response-spec.js
+++ test/spec/config-response-spec.js
@@ -1,10 +1,70 @@
 describe('配置 response', function() {
-    it('GET /api/configdemo-response/data', function(done) {
+    it('GET /api/configdemo-response/number', function(done) {
         $.ajax({
             type: 'GET',
-            url: API_ROOT + '/api/configdemo-response/data',
+            url: API_ROOT + '/api/configdemo-response/number',
         }).then(function(result) {
-            expect(result.route).toBe('GET /api/configdemo-response/data');
+            expect(result).toBe(1);
+            done();
+        });
+    });
+
+    it('GET /api/configdemo-response/boolean', function(done) {
+        $.ajax({
+            type: 'GET',
+            url: API_ROOT + '/api/configdemo-response/boolean',
+        }).then(function(result) {
+            expect(result).toBe(true);
+            done();
+        });
+    });
+
+    it('GET /api/configdemo-response/string', function(done) {
+        $.ajax({
+            type: 'GET',
+            url: API_ROOT + '/api/configdemo-response/string',
+        }).then(function(result) {
+            expect(result).toBe('a');
+            done();
+        });
+    });
+
+    it('GET /api/configdemo-response/object', function(done) {
+        $.ajax({
+            type: 'GET',
+            url: API_ROOT + '/api/configdemo-response/object',
+        }).then(function(result) {
+            expect(result.route).toBe('GET /api/configdemo-response/object');
+            done();
+        });
+    });
+
+    it('GET /api/configdemo-response/array', function(done) {
+        $.ajax({
+            type: 'GET',
+            url: API_ROOT + '/api/configdemo-response/array',
+        }).then(function(result) {
+            expect(result).toEqual([1, true, false, "a", {"a": 1}, [1, 2], null]);
+            done();
+        });
+    });
+
+    it('GET /api/configdemo-response/null', function(done) {
+        $.ajax({
+            type: 'GET',
+            url: API_ROOT + '/api/configdemo-response/null',
+        }).then(function(result) {
+            expect(result).toBe(null);
+            done();
+        });
+    });
+
+    it('GET /api/configdemo-response/mock-simple', function(done) {
+        $.ajax({
+            type: 'GET',
+            url: API_ROOT + '/api/configdemo-response/mock-simple',
+        }).then(function(result) {
+            expect(typeof result).toBe('string');
             done();
         });
     });
